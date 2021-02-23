e28da0ec12cb11bffe4e26fcedc7e40888fd49fb
idansheffer <idan.sheffer@gmail.com>
2021-02-23 17:36:38 +0200

    fixed nets

--- project/net_models.py
+++ project/net_models.py
@@ -7,13 +7,13 @@ class CompoundNet(nn.Module):
         super(CompoundNet, self).__init__()
         self.appearance_net = AppearanceNet()
         self.structural_net = StructuralNet()
-        self.global_avg_pooling_2d = nn.AdaptiveAvgPool2d((1, 128))
-        self.global_avg_pooling_3d = nn.AdaptiveAvgPool2d((1, 1, 128))
+        self.global_avg_pooling_2d = nn.AdaptiveAvgPool2d(1)
+        self.global_avg_pooling_3d = nn.AdaptiveAvgPool3d(1)
         self.fusion_net = FusionNet()
 
     def forward(self, I, G):
-        I = self.global_avg_pooling_2d(self.appearance_net(I))
-        G = self.global_avg_pooling_3d(self.structural_net(G))
+        I = self.global_avg_pooling_2d(self.appearance_net(I)).squeeze()
+        G = self.global_avg_pooling_3d(self.structural_net(G)).squeeze()
         return self.fusion_net(I, G)
 
 
@@ -23,16 +23,39 @@ class AppearanceNet(nn.Module):
         self.conv_section = nn.Sequential(
             ConvBlock2D(in_channels=3, out_channels=64),
             ConvBlock2D(in_channels=64, out_channels=64),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             ConvBlock2D(in_channels=64, out_channels=64),
             ConvBlock2D(in_channels=64, out_channels=64),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             ConvBlock2D(in_channels=64, out_channels=64),
             ConvBlock2D(in_channels=64, out_channels=64),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             ConvBlock2D(in_channels=64, out_channels=128),
             ConvBlock2D(in_channels=128, out_channels=128),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             ConvBlock2D(in_channels=128, out_channels=128),
             ConvBlock2D(in_channels=128, out_channels=128),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             ConvBlock2D(in_channels=128, out_channels=128),
             ConvBlock2D(in_channels=128, out_channels=128)
+            
+            # ConvBlock2D(in_channels=3, out_channels=64),
+            # ConvBlock2D(in_channels=64, out_channels=64),
+            # nn.MaxPool2d(kernel_size=2, stride=2),
+            # ConvBlock2D(in_channels=64, out_channels=64),
+            # ConvBlock2D(in_channels=64, out_channels=64),
+            # nn.MaxPool2d(kernel_size=2, stride=2),
+            # ConvBlock2D(in_channels=64, out_channels=64),
+            # ConvBlock2D(in_channels=64, out_channels=128),
+            # nn.MaxPool2d(kernel_size=2, stride=2),
+            # ConvBlock2D(in_channels=64, out_channels=128),
+            # ConvBlock2D(in_channels=128, out_channels=128),
+            # nn.MaxPool2d(kernel_size=2, stride=2),
+            # ConvBlock2D(in_channels=128, out_channels=128),
+            # ConvBlock2D(in_channels=128, out_channels=128),
+            # nn.MaxPool2d(kernel_size=2, stride=2),
+            # ConvBlock2D(in_channels=128, out_channels=256),
+            # ConvBlock2D(in_channels=128, out_channels=256)
         )
 
     def forward(self, x):
@@ -45,13 +68,31 @@ class StructuralNet(nn.Module):
         self.conv_section = nn.Sequential(
             ConvBlock3D(in_channels=1, out_channels=32),
             ConvBlock3D(in_channels=32, out_channels=32),
+            nn.AvgPool3d(kernel_size=2, stride=2),
             ConvBlock3D(in_channels=32, out_channels=64),
             ConvBlock3D(in_channels=64, out_channels=64),
+            nn.AvgPool3d(kernel_size=2, stride=2),
             ConvBlock3D(in_channels=64, out_channels=64),
             ConvBlock3D(in_channels=64, out_channels=64),
+            nn.AvgPool3d(kernel_size=2, stride=2),
             ConvBlock3D(in_channels=64, out_channels=128),
             ConvBlock3D(in_channels=128, out_channels=128),
+            nn.AvgPool3d(kernel_size=2, stride=2),
             ConvBlock3D(in_channels=128, out_channels=128)
+            
+            # ConvBlock3D(in_channels=1, out_channels=32),
+            # ConvBlock3D(in_channels=32, out_channels=32),
+            # nn.AvgPool3d(kernel_size=2, stride=2),
+            # ConvBlock3D(in_channels=32, out_channels=64),
+            # ConvBlock3D(in_channels=64, out_channels=64),
+            # nn.AvgPool3d(kernel_size=2, stride=2),
+            # ConvBlock3D(in_channels=64, out_channels=64),
+            # ConvBlock3D(in_channels=64, out_channels=128),
+            # nn.AvgPool3d(kernel_size=2, stride=2),
+            # ConvBlock3D(in_channels=64, out_channels=128),
+            # ConvBlock3D(in_channels=128, out_channels=256),
+            # nn.AvgPool3d(kernel_size=2, stride=2),
+            # ConvBlock3D(in_channels=128, out_channels=256)
         )
 
     def forward(self, x):
@@ -59,12 +100,11 @@ class StructuralNet(nn.Module):
 
 
 class ConvBlock2D(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=0):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
         super(ConvBlock2D, self).__init__()
         self.conv_block = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.ReLU()
         )
 
     def forward(self, x):
@@ -72,12 +112,11 @@ class ConvBlock2D(nn.Module):
 
 
 class ConvBlock3D(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=0):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
         super(ConvBlock3D, self).__init__()
         self.conv_block = nn.Sequential(
             nn.Conv3d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
-            nn.ReLU(),
-            nn.AvgPool3d(kernel_size=2, stride=2)
+            nn.ReLU()
         )
 
     def forward(self, x):
@@ -88,4 +127,4 @@ class FusionNet(nn.Module):
         super(FusionNet, self).__init__()
     
     def forward(self, I, G):
-        return [I, G]
+        return torch.cat((I, G), 1)
