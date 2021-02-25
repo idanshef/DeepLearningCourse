b14073bd25aef0c27f4fdff985b75d3dfc536b76
idansheffer <idan.sheffer@intel.com>
2021-02-25 18:57:42 +0200

    implemented build_samples_list and is_match functions

--- project/utils.py
+++ project/utils.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 
-def create_voxel_grid_from_point_cloud(point_cloud, center_m, grid_resolution = (96, 96, 48), volume_size = (40, 40, 20)):
+def create_voxel_grid_from_point_cloud(point_cloud, grid_resolution = (96, 96, 48), volume_size = (40, 40, 20)):
+    center_m = np.zeros(3)
     voxel_grid = np.zeros(grid_resolution)
     filter_point_cloud = abs(point_cloud - np.tile(center_m, (point_cloud.shape[0], 1)))
     
@@ -17,3 +18,47 @@ def create_voxel_grid_from_point_cloud(point_cloud, center_m, grid_resolution = 
     np.add.at(voxel_grid, tuple(points_axis.T), 1)
     
     return voxel_grid
+
+def build_samples_list(data_dir, structure_time_span):
+    data_list = []
+    full_gps_df_rad = None
+    for data_date in os.listdir(data_dir):
+        date_path = os.path.join(data_dir, data_date)
+        lidar_dir = os.path.join(date_path, 'ldmrs')
+        img_dir = os.path.join(date_path, 'stereo', 'centre')
+        poses_file_path = os.path.join(date_path, "gps", "ins.csv")
+        gps_df = pd.read_csv(poses_file_path)
+        
+        models_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "models")
+        camera_model = CameraModel(models_dir, img_dir)
+        
+        for img_name in os.listdir(img_dir):
+            data_dict = dict()
+            data_dict['date'] = data_date
+            data_dict['lidar_dir'] = lidar_dir
+            data_dict['camera'] = camera_model
+            data_dict['I'] = os.path.join(img_dir, img_name)
+            data_dict['poses_path'] = poses_file_path
+            
+            img_timestamp = int(img_name[:-4])
+            data_dict['start_time'] = str(curr_timestamp + val - round(structure_time_span/2) * 1e6)
+            data_dict['end_time'] = str(curr_timestamp + val + round(structure_time_span/2) * 1e6)
+            
+            closest_time_idx = abs(gps_df['timestamp'] - img_timestamp).argmin()
+            data_dict['latitude'] = gps_df['latitude'][closest_time_idx]
+            data_dict['longitude'] = gps_df['longitude'][closest_time_idx]
+
+            data_list.append(data_dict)
+            
+            curr_lat_long = np.array(list(map(np.radians, [data_dict['latitude'], data_dict['longitude']])))
+            if full_gps_df_rad is None:
+                full_gps_df_rad = curr_lat_long
+            else:
+                full_gps_df_rad = np.concatenate(full_gps_df_rad, curr_lat_long)
+    
+    return data_list, full_gps_df_rad
+
+def is_match(Xi, Xj, threshold_m):
+    Xi_lat_long = (Xi['latitude'], Xi['logitude'])
+    Xj_lat_long = (Xj['latitude'], Xj['logitude'])
+    return distance(Xi_lat_long, Xj_lat_long).m <= threshold_m
